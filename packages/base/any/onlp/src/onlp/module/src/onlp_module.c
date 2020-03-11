d18f3f9aa940455af243da5a2c90024b7c1f6fbe
John Ingram <john.ingram@adtran.com>
2020-03-11 08:29:20 -0500

    Removed some incorrect usage of extern keyword

--- packages/base/any/onlp/src/onlp/module/src/onlp_module.c
+++ packages/base/any/onlp/src/onlp/module/src/onlp_module.c
@@ -86,10 +86,9 @@ void __onlp_module_init__(void)
 #endif
 
     {
-        extern int __onlp_platform_version__;
-        extern int __onlp_platform_version_default__;
-        __onlp_platform_version_default__ = 1;
-        __onlp_platform_version__= 1;
+        int __onlp_platform_version__ = 1;
+        int __onlp_platform_version_default__ = 1;
+
     }
 }
 
