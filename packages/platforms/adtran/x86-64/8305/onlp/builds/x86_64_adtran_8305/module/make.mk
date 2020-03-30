bfa3c33d5b30945e9a2b7e1b6a906229d179161b
sboyett <stephen.boyett@adtran.com>
2020-03-30 18:31:48 +0000

    Makefile fix in make.mk

--- packages/platforms/adtran/x86-64/8305/onlp/builds/x86_64_adtran_8305/module/make.mk
+++ packages/platforms/adtran/x86-64/8305/onlp/builds/x86_64_adtran_8305/module/make.mk
@@ -6,5 +6,5 @@
 THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 x86_64_adtran_8305_INCLUDES := -I $(THIS_DIR)inc
 x86_64_adtran_8305_INTERNAL_INCLUDES := -I $(THIS_DIR)src
-x86_64_adtran_8305_DEPENDMODULE_ENTRIES := init:x86_64_accton_as7712_32x ucli:x86_64_accton_as7712_32x
+x86_64_adtran_8305_DEPENDMODULE_ENTRIES := init:x86_64_adtran_8305 ucli:x86_64_adtran_8305
 
